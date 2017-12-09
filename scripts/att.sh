e560e471d23e67f8aa16fd17773f9f1759d8be7c
Leonn34 <eltonsousadesigner@gmail.com>
2017-12-08 23:40:29 -0400

    att menu

--- scripts/att.sh
+++ scripts/att.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+
+#############################################
+corPadrao="\033[0m"
+preto="\033[0;30m"
+vermelho="\033[0;31m"
+verde="\033[0;32m"
+marrom="\033[0;33m"
+azul="\033[0;34m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+cinzaClaro="\033[0;37m"
+pretoCinza="\033[1;30m"
+vermelhoClaro="\033[1;31m"
+verdeClaro="\033[1;32m"
+amarelo="\033[1;33m"
+azulClaro="\033[1;34m"
+purpleClaro="\033[1;35m"
+cyanClaro="\033[1;36m"
+branco="\033[1;37m"
+fim="\033[0;37m"
+#############################################
+
 clear
 echo ""
-echo "Aguarde...."
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $vermelhoClaro"Aguarde...."$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 5
 clear
 echo ""
@@ -14,18 +38,24 @@ apt-get upgrade -y 1>/dev/null 2>/dev/null
 #cd /bin
 
 #REMOVENDO ARQUIVOS
-echo "Removendo Arquivos..."
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $vermelhoClaro"Removendo Arquivos..."$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 5
 clear
 
 rm /bin/criarusuario /bin/expcleaner /bin/sshlimiter /bin/addhost /bin/sshmonitor /bin/ajuda /bin/menu /bin/menu.sh /bin/atualizar /bin/clearcache /bin/badudp /bin/userbkp /bin/socks.py /bin/setup.sh /bin/att 1>/dev/null 2>/dev/null
 rm /root/ExpCleaner.sh /root/CriarUsuario.sh /root/sshlimiter.sh 1>/dev/null 2>/dev/null
-echo "Arquivos Removidos!"
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $vermelhoClaro"Arquivos Removidos!"$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 5
 clear
 
 #BAIXANDO SCRIPTS
-echo "Baixando novos arquivos..."
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $vermelhoClaro"Baixando novos arquivos..."$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 5
 clear
 
@@ -70,7 +100,9 @@ chmod +x /bin/att
 rm /bin/setup.sh 1>/dev/null 2>/dev/null
 clear
 
-echo "Download concluido!"
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $verdeClaro"Download concluido!"$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 5
 cd
 sleep 2
@@ -94,21 +126,25 @@ if [ ! -f "/etc/init.d/ssh" ]
 fi
 clear
 echo ""
-echo "Atualizado com sucesso!"
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $verdeClaro"Atualizado com sucesso!"$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 5
 echo ""
-echo "Entrando no menu em:..."
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo"Entrando no menu em:..."$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 sleep 1
 echo ""
-echo "5....."
+echo -e $verdeClaro"5....."$fim
 sleep 1
-echo "4...."
+echo -e $verdeClaro"4...."$fim
 sleep 1
-echo "3..."
+echo -e $verdeClaro"3..."$fim
 sleep 1
-echo "2.."
+echo -e $verdeClaro"2.."$fim
 sleep 1
-echo "1."
+echo -e $verdeClaro"1."$fim
 sleep 1
 clear
 exit1
