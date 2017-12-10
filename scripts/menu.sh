f45424a8d468d3cf4fbedbf1c0a88a8aa65e5810
Leonn34 <eltonsousadesigner@gmail.com>
2017-12-10 19:12:25 -0400

    att sshlimiter

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -171,13 +171,23 @@ fi
       sshmonitor
       ;;
     10)
+      clear
       echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
-      echo "PARA ATIVAR O LIMITE DE CONEXÔES:"
-      echo "ABRA UMA SESSÃO SCREEN, DIGITE: screen E APERTE ENTER."
-      echo "DIGITE: sshlimiter"
-      echo "PARA SAIR E DEIXAR RODANDO, DIGITE: CTRL+A+D"
-      echo "OBS. REPITA ESSES PROCESSOS A CADA VEZ QUE REINICIAR A VPS!"
-      echo "ENTER para voltar"
+      echo -e $amarelo"PARA ATIVAR O LIMITE DE CONEXÔES:"$fim
+      sleep 2
+      echo ""
+      echo -e $branco"ABRA UMA SESSÃO"$fim $verdeClaro"SCREEN,"$fim $branco"DIGITE:"$fim $verdeClaro"screen"$fim $branco"E APERTE"$fim $verdeClaro"ENTER."$fim
+      sleep 2
+      echo -e $branco"DIGITE:"$fim $verdeClaro"sshlimiter"$fim
+      sleep 2
+      echo -e $branco"PARA SAIR E DEIXAR RODANDO, DIGITE:"$fim $verdeClaro"CTRL+A+D"$fim
+      sleep 2
+      echo ""
+      echo -e $amarelo"OBS. REPITA ESSES PROCESSOS A CADA VEZ QUE REINICIAR A VPS!"$fim
+      echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+      sleep 2
+      echo ""
+      echo -e $verdeClaro"ENTER"$fim $branco"para voltar"$fim
       read -p ""
       sleep 1
       menu
